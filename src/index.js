968750d0fba31426d13ce91b23876206ea1c1876
i-serra <104382671+i-serra@users.noreply.github.com>
2022-08-01 23:34:52 +0100

    Added link to git hub

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import Header from "./Header";
+import "./index.css";
 
 function App() {
   return (
     <div>
       <Header />
+      <p className="code-source">
+        <a href="https://github.com/i-serra/weather-app-react">Open-sourced code</a> by Inês Serra
+      </p>
     </div>
   );
 }
@@ -22,4 +26,3 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-
